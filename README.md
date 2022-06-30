6fc5d261eba651fc13e599300c859e55a6e76eb6
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-06-30 14:26:15 +0600

    fixed login issue

---
+++ README.md
@@ -0,0 +1,54 @@
+# K Task TODO Web App
+
+### It's a simple React base Web App.
+
+### Here you can see a ToDo app for TODO list.
+
+---
+
+## The following things in this project:
+
+- Home
+- Login
+- TODO (Specific User)
+- Other's data.
+
+---
+
+### You can see me here.. [Click Me](https://k-task-todo.web.app/)
+
+## Installation
+
+```bash
+git clone https://github.com/kiron0/To-Do-Client.git
+```
+
+```bash
+cd To-Do-Client
+```
+
+```bash
+yarn
+```
+
+```bash
+code .
+```
+
+```bash
+yarn start
+```
+
+---
+
+## Prerequisites
+
+The following open source packages are used in this project:
+
+- React JS v18
+- TailwindCSS
+- DaisyUI
+- React Router DOM v6
+- React Icons
+
+---
---
+++ src/components/Login/Login/Login.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useNavigate, useLocation } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-hot-toast";
+import Loading from "../../Loading/Loading";
+import auth from "../../Firebase/firebase.init";
+import useToken from "../../../hooks/useToken";
+
+const Login = () => {
+  const [signInWithGoogle, gUser, gLoading] = useSignInWithGoogle(auth);
+  const [token] = useToken(gUser);
+  const navigate = useNavigate();
+  const location = useLocation();
+  let from = location.state?.from?.pathname || "/";
+
+  useEffect(() => {
+    if (token) {
+      navigate(from, { replace: true });
+      toast.success(`Welcome Back, ${auth?.currentUser?.displayName}`, {
+        autoClose: 4000,
+      });
+    }
+  }, [token, navigate, from]);
+
+  if (gLoading) {
+    return <Loading></Loading>;
+  }
+
+  if (token) {
+    navigate(from, { replace: true });
+  }
+
+  return (
+    <div className="bg-base-100 h-screen">
+      <div className="flex h-96 justify-center items-center px-4 lg:px-12">
+        <div className="card w-full max-w-md bg-base-100">
+          <div className="card-body">
+            <h2 className="text-center text-4xl font-bold pb-10">Login</h2>
+            <button
+              onClick={() => signInWithGoogle()}
+              className="btn btn-outline border-primary flex items-center content-center rounded-full hover:btn-primary"
+            >
+              <FcGoogle className="text-2xl mr-2"></FcGoogle>Continue with
+              Google
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
